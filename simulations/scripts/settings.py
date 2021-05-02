bd270908ee93fbef1530400d47570ce5a05e65a8
tohein <tbheinen@gmail.com>
2021-05-02 21:54:00 +0200

    simulation default param update

--- simulations/scripts/settings.py
+++ simulations/scripts/settings.py
@@ -7,19 +7,19 @@ import os
 DEFAULT_PARAMS = {
     'n_snps': 25,
     'n_individuals': 100,
-    'n_genes': 50,
+    'n_genes': 100,
     'cells_per_individual': 'fixed',
     'maf_min': 0.2,
     'maf_max': .45,
     'env': 'endo',
     'd_env': 10,
-    'offset': .3,
+    'offset': 2.5,
     'n_causal_g': 1,
     'n_causal_gxe': 1,
     'r0': .2, # fraction of genetic variance explained by gxe
     'v0': .1, # total variance explained by genetics
     'likelihood': 'gaussian',
-    'nb_dispersion': 1,
+    'nb_dispersion': 1.5,
     'p_dropout': 0.05,
     'normalize': False,
     'dirichlet_alpha': 5,
